425b141f783251fdea5188557eab4de2281ff6f1
Björn Söderqvist <cybear@gmail.com>
2020-12-25 15:46:56 +0100

    Some kind of changes

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,12 @@ pub struct WordPredictions {
     word: String,
     predictions: Vec<(String, usize)>,
 }
+struct WordScore {
+    word: String,
+    second_word: String,
+    score: usize,
+}
+
 impl fmt::Display for WordPredictions {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(
@@ -33,29 +39,33 @@ pub fn parse_file(s: &str) -> Vec<String> {
     ).collect()
 }
 
-fn generate_scores(words: &[String]) -> HashMap<String, usize> {
+fn generate_scores(words: &[String]) -> HashMap<String, WordScore> {
     println!("Generating scores for {} sequences", words.len());
     let mut prediction_map: HashMap<String, usize> = HashMap::new();
     let iter = words.windows(2);
     let keys = iter.map(|pair| format!("{}§{}", pair[0], pair[1]));
-    let val: usize = 1;
     for key in keys {
-        let key = key.clone();
-        if !prediction_map.contains_key(key.as_str()) {
-            prediction_map.insert(key, val);
-        } else {
-            let val = prediction_map.get(key.as_str()).unwrap() + 1;
-            prediction_map.insert(key, val);
-        }
+        *prediction_map.entry(key).or_insert(0) += 1;
     }
-    prediction_map
+    // At least only do this ONCE
+    prediction_map.iter().map(|(key, score)| {
+        let mut split = key.split("§");
+        let word = split.next().unwrap().to_string();
+        let second_word = split.next().unwrap().to_string();
+        let score = *score;
+        (key.to_string(), WordScore {
+            word,
+            second_word,
+            score,
+        })
+    }).collect()
 }
 
-fn clean_scores(map: HashMap<String, usize>, minimum: usize) -> HashMap<String, usize> {
-    map.into_iter()
-        .filter(|&(_, v)| v >= minimum)
-        .collect()
-}
+// fn clean_scores(map: HashMap<String, usize>, minimum: usize) -> HashMap<String, usize> {
+//     map.into_iter()
+//         .filter(|&(_, v)| v >= minimum)
+//         .collect()
+// }
 
 fn get_unique_words(words: &Vec<String>) -> Vec<String> {
     println!("Deduping {} words", words.len());
@@ -65,30 +75,29 @@ fn get_unique_words(words: &Vec<String>) -> Vec<String> {
     words_sorted
 }
 
-fn keyval_hashmap_to_wordprediction_hashmap(unique_words: Vec<String>, predictions_map: HashMap<String, usize>) -> HashMap<String, WordPredictions> {
+fn keyval_hashmap_to_wordprediction_hashmap(
+    unique_words: Vec<String>,
+    predictions_map: HashMap<String, WordScore>
+) -> HashMap<String, WordPredictions> {
     println!("Generating word predictions for {} words", unique_words.len());
     unique_words
         .par_iter()
         .map(|first_word| {
-            let prefix = format!("{}§", first_word);
-            let second_word_keys = predictions_map
-                .keys()
-                .filter(|word| word.starts_with(&prefix));
-            let mut second_word_scores: Vec<(String, usize)> = second_word_keys
-                .map(|key| {
-                    let mut split = key.split("§");
-                    split.next();
-                    let second_word = split.next().unwrap();
-                    (second_word.to_string(), predictions_map.get(key).unwrap().clone())}
-                )
+            let mut second_word_scores: Vec<&WordScore> = predictions_map
+                .values()
+                .filter(|word_score| word_score.word == *first_word)
                 .collect();
-            second_word_scores.sort_by(|a, b| b.1.cmp(&a.1));
+            second_word_scores.sort_by(|a, b| b.score.cmp(&a.score));
             (
                 first_word.to_string(),
                 WordPredictions {
-                word: first_word.to_string(),
-                predictions: second_word_scores,
-            })
+                    word: first_word.to_string(),
+                    predictions: second_word_scores
+                        .iter()
+                        .map(|word_score| (word_score.second_word.clone(), word_score.score))
+                        .collect(),
+                }
+            )
         })
         .collect()
 }
@@ -109,11 +118,9 @@ fn test_parse_file() {
 fn test_generate_n_grams() {
     let words = parse_file(TESTDATA);
     let two_grams = generate_scores(&words);
-    let score1: usize = 1;
-    let score2: usize = 2;
-    assert_eq!(two_grams.get("i§am").unwrap(), &score2);
-    assert_eq!(two_grams.get("am§a").unwrap(), &score2);
-    assert_eq!(two_grams.get("a§fish").unwrap(), &score1);
+    assert_eq!(two_grams.get("i§am").unwrap().word, "i");
+    assert_eq!(two_grams.get("i§am").unwrap().second_word, "am");
+    assert_eq!(two_grams.get("i§am").unwrap().score, 2);
 }
 
 #[test]
@@ -146,8 +153,7 @@ fn test_bible_parser() {
     assert_eq!(words.len(), 858338);
     let unique_words = get_unique_words(&words);
     let two_grams = generate_scores(&words);
-    let two_grams_score_above_1 = clean_scores(two_grams, 2);
-    let word_predictions = keyval_hashmap_to_wordprediction_hashmap(unique_words, two_grams_score_above_1);
+    let word_predictions = keyval_hashmap_to_wordprediction_hashmap(unique_words, two_grams);
     let word_a = word_predictions.get("a").unwrap();
     assert_eq!(word_a.word, "a");
     assert_eq!(word_a.predictions.len(), 795);
