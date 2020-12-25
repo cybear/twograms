9512a60c3c532114f85ec3c6ca63a1d6fc101354
Björn Söderqvist <cybear@gmail.com>
2020-12-25 11:43:28 +0100

    Use less common split character

--- src/lib.rs
+++ src/lib.rs
@@ -35,7 +35,7 @@ fn generate_scores(words: &[String]) -> HashMap<String, usize> {
     println!("Generating scores for {} sequences", words.len());
     let mut prediction_map: HashMap<String, usize> = HashMap::new();
     let iter = words.windows(2);
-    let keys = iter.map(|pair| format!("{}:{}", pair[0], pair[1]));
+    let keys = iter.map(|pair| format!("{}§{}", pair[0], pair[1]));
     let val: usize = 1;
     for key in keys {
         let key = key.clone();
@@ -60,22 +60,24 @@ fn get_unique_words(words: &Vec<String>) -> Vec<String> {
 fn keyval_hashmap_to_wordpredictions(unique_words: Vec<String>, predictions_map: HashMap<String, usize>) -> Vec<WordPredictions> {
     println!("Generating word predictions for {} words", unique_words.len());
     unique_words.iter().map(|first_word| {
-        let prefix = format!("{}:", first_word);
+        let prefix = format!("{}§", first_word);
         let second_word_keys = predictions_map.keys()
             .filter(|word| word.starts_with(&prefix));
         let mut second_word_scores: Vec<(String, usize)> = second_word_keys
             .map(|key| {
-                let mut split = key.split(":");
+                let mut split = key.split("§");
                 split.next();
                 let second_word = split.next().unwrap();
                 (second_word.to_string(), predictions_map.get(key).unwrap().clone())}
             )
             .collect();
         second_word_scores.sort_by(|a, b| b.1.cmp(&a.1));
-        WordPredictions {
+        let w = WordPredictions {
             word: first_word.to_string(),
             predictions: second_word_scores,
-        }
+        };
+        println!("Generated word: {}", w);
+        w
     }).collect()
 }
 
@@ -97,9 +99,9 @@ fn test_generate_n_grams() {
     let two_grams = generate_scores(&words);
     let score1: usize = 1;
     let score2: usize = 2;
-    assert_eq!(two_grams.get("i:am").unwrap(), &score2);
-    assert_eq!(two_grams.get("am:a").unwrap(), &score2);
-    assert_eq!(two_grams.get("a:fish").unwrap(), &score1);
+    assert_eq!(two_grams.get("i§am").unwrap(), &score2);
+    assert_eq!(two_grams.get("am§a").unwrap(), &score2);
+    assert_eq!(two_grams.get("a§fish").unwrap(), &score1);
 }
 
 #[test]
@@ -123,18 +125,17 @@ fn test_alice() {
     println!("{}", word_predictions[1]);
     println!("{}", word_predictions[2]);
     assert_eq!(word_predictions[0].word, "a");
+    assert_eq!(word_predictions[0].predictions.len(), 37);    
     assert_eq!(word_predictions.len(), 610);
 }
 
-#[test]
-fn test_bible_parser() {
-    let words = parse_file(include_str!("10900-8.txt"));
-    let unique_words = get_unique_words(&words);
-    let two_grams = generate_scores(&words);
-    // let word_predictions = keyval_hashmap_to_wordpredictions(two_grams);
-    // println!("{}", word_predictions[0]);
-    // println!("{}", word_predictions[1]);
-    // println!("{}", word_predictions[2]);
-    assert_eq!(words.len(), 858338);
-}
+// #[test]
+// fn test_bible_parser() {
+//     let words = parse_file(include_str!("10900-8.txt"));
+//     let unique_words = get_unique_words(&words);
+//     let two_grams = generate_scores(&words);
+//     let word_predictions = keyval_hashmap_to_wordpredictions(unique_words, two_grams);
+//     println!("{}", word_predictions[0]);
+//     assert_eq!(words.len(), 858338);
+// }
 }
