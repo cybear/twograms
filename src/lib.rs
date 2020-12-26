b6e8323c95ead203b5432127d7e73d02adfb39e9
Björn Söderqvist <cybear@gmail.com>
2020-12-26 18:38:28 +0100

    Sort values

--- src/lib.rs
+++ src/lib.rs
@@ -20,25 +20,30 @@ pub fn parse_file(s: &str) -> Vec<String> {
 fn generate_scores(words: &[String]) -> HashMap<(String, String), usize> {
     println!("Generating scores for {} sequences", words.len());
     let mut prediction_map: HashMap<(String, String), usize> = HashMap::new();
-    words
-        .windows(2)
-        .for_each(|pair| {
-            *prediction_map
-                .entry((pair[0].clone(), pair[1].clone()))
-                .or_insert(0) += 1;
-        });
+    words.windows(2).for_each(|pair| {
+        *prediction_map
+            .entry((pair[0].clone(), pair[1].clone()))
+            .or_insert(0) += 1;
+    });
     prediction_map
 }
 
-fn group_wordpredictions(predictions_hm: HashMap<(String, String), usize>) -> HashMap<String, Vec<(String, usize)>> {
+fn group_wordpredictions(
+    predictions_hm: HashMap<(String, String), usize>,
+) -> HashMap<String, Vec<(String, usize)>> {
     let mut hm: HashMap<String, Vec<(String, usize)>> = HashMap::new();
     for ((first_word, second_word), score) in predictions_hm {
-        let entry = hm
-            .entry(first_word)
-            .or_insert(vec![]);
+        let entry = hm.entry(first_word).or_insert(vec![]);
         entry.push((second_word, score));
     }
-    hm
+    // Sort the items by score
+    let mut hm_sorted: HashMap<String, Vec<(String, usize)>> = HashMap::new();
+    for (first_word, arr) in hm {
+        let mut sorted = arr.clone();
+        sorted.sort_by(|(_a1, a2), (_b1, b2)| b2.cmp(a2));
+        hm_sorted.insert(first_word, sorted);
+    }
+    hm_sorted
 }
 
 #[cfg(test)]
@@ -61,6 +66,25 @@ mod tests {
         let word_a = word_predictions.get("a").unwrap();
         assert_eq!(word_a.len(), 37);
         assert_eq!(word_predictions.len(), 610);
+        assert_eq!(word_a[0].0, "little");
+    }
+
+    #[test]
+    fn test_hhgttg() {
+        let words = parse_file(include_str!("hhgttg.txt"));
+        let scores = generate_scores(&words);
+        let word_predictions = group_wordpredictions(scores);
+        let word_a = word_predictions.get("a").unwrap();
+        println!(
+            "{}",
+            word_a
+                .iter()
+                .map(|(w, s)| format!("{}:{}", w, s))
+                .collect::<Vec<String>>()
+                .join(",")
+        );
+        assert_eq!(word_a.len(), 37);
+        assert_eq!(word_predictions.len(), 610);
     }
 
     #[test]
@@ -71,5 +95,6 @@ mod tests {
         let word_predictions = group_wordpredictions(scores);
         let word_a = word_predictions.get("a").unwrap();
         assert_eq!(word_a.len(), 1335);
+        assert_eq!(word_a[0].0, "man");
     }
 }
