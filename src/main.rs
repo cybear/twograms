15fe8c2b2037b4b0e9e57781b504a284804c2473
Björn Söderqvist <cybear@gmail.com>
2021-01-07 10:48:59 +0100

    Add option for how many suggestions to keep

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,58 @@
-use std::{env, fs};
+use std::error::Error;
+use std::{env, fs, process};
 mod lib;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    if args.len() != 2 {
-        panic!("Need to pass (only) filename");
+    let config = Config::new(env::args()).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
+        println!("usage: twograms <filename> [<max-suggestions>]");
+        println!(
+            "       <filename>           The text file used to generate the n-grams structure"
+        );
+        println!("       [<max-suggestions>]  An optional number between 1-5");
+        process::exit(1);
+    });
+
+    if let Err(e) = run(config) {
+        println!("Application error: {}", e);
+
+        process::exit(1);
     }
-    let filename = &args[1];
-    let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
-    let json = lib::to_json(&contents);
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let filename = config.filename;
+    let text = fs::read_to_string(filename)?;
+    let ngrams = lib::generate_ngrams(&text, config.keep);
+    let json = serde_json::to_string(&ngrams).unwrap();
     println!("{}", json);
+    Ok(())
+}
+
+struct Config {
+    filename: String,
+    keep: usize,
+}
+
+impl Config {
+    fn new(mut args: env::Args) -> Result<Config, &'static str> {
+        args.next(); // the program name is the first argument
+        let filename = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file name"),
+        };
+        let keep = match args.next() {
+            Some(arg) => match arg.as_str() {
+                "1" => 1,
+                "2" => 2,
+                "3" => 3,
+                "4" => 4,
+                "5" => 5,
+                _ => 0,
+            },
+            None => 0,
+        };
+
+        Ok(Config { filename, keep })
+    }
 }
