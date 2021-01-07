5e98cf17534f0a8980775e3a53fc2ab1a5f7c224
Björn Söderqvist <cybear@gmail.com>
2021-01-07 17:15:13 +0100

    WiP

--- benches/generate.rs
+++ benches/generate.rs
@@ -1,46 +1,33 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use twograms::{generate_ngrams, generate_scores, group_wordpredictions, parse_file};
 
-fn criterion_parse(c: &mut Criterion) {
-    let bible = include_str!("10900-8.txt");
-    c.bench_function("parse_file for bible", |b| {
-        b.iter(|| parse_file(black_box(bible)))
-    });
-}
-
-fn criterion_generate_scores(c: &mut Criterion) {
-    let bible = include_str!("10900-8.txt");
-    let words = parse_file(bible);
-    c.bench_function("generate_scores for bible", |b| {
-        b.iter(|| generate_scores(black_box(words.clone())))
-    });
-}
-
-fn criterion_group_wordpredictions(c: &mut Criterion) {
-    let bible = include_str!("10900-8.txt");
-    let words = parse_file(bible);
-    let scores = generate_scores(words);
-
-    c.bench_function("group_wordpredictions for bible", |b| {
-        b.iter(|| group_wordpredictions(black_box(scores.clone()), 100000))
-    });
-}
-
 fn criterion_generate(c: &mut Criterion) {
     let bible = include_str!("10900-8.txt");
-    c.bench_function("Generate for bible", |b| {
+    let words = parse_file(&bible);
+    let scores = generate_scores(parse_file(&bible));
+
+    let mut group = c.benchmark_group("twograms lib");
+    // group.significance_level(0.1).sample_size(10);
+
+    group.bench_function("parse_file for bible", |b| {
+        b.iter(|| parse_file(black_box(bible)))
+    });
+    group.bench_function("generate_scores for bible", |b| {
+        b.iter(|| generate_scores(black_box(words.clone())))
+    });
+    group.bench_function("group_wordpredictions for bible", |b| {
+        b.iter(|| group_wordpredictions(black_box(scores.clone()), 100000))
+    });
+    group.bench_function("Generate for bible", |b| {
         b.iter(|| generate_ngrams(black_box(bible), 100000))
     });
+    group.finish();
 }
 
 criterion_group!{
     name = benches;
-    config = Criterion::default().sample_size(20);
-    targets = 
-        criterion_parse,
-        criterion_generate,
-        criterion_generate_scores,
-        criterion_group_wordpredictions,
+    config = Criterion::default(); //.sample_size(20);
+    targets = criterion_generate,
 }
 
 criterion_main!(benches);
