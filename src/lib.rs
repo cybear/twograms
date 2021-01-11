892643f51afe1009f01d397b6b9471ab656f0232
Björn Söderqvist <bjorn.soderqvist@ingka.com>
2021-01-11 16:00:13 +0100

    Change into iterator

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use rayon::prelude::*;
+// use rayon::prelude::*;
 use serde::Serialize;
 use std::collections::HashMap;
 #[derive(Clone, Serialize, Debug)]
@@ -10,16 +10,19 @@ pub fn generate_ngrams(text: &str, keep: usize) -> HashMap<String, Vec<WordPropo
     group_wordpredictions(generate_scores(parse_file(text)), keep)
 }
 
-pub fn parse_file(s: &str) -> Vec<Vec<String>> {
-    s.as_parallel_string()
-        .par_split(|c: char| c.is_ascii_punctuation())
-        .map(|s| s.split_whitespace().map(|w| w.to_lowercase()).collect())
-        .collect()
+fn parse_line(s: &str) -> Vec<String> {
+    s.split_whitespace().map(|w| w.to_lowercase()).collect()
 }
 
-pub fn generate_scores(sentences: Vec<Vec<String>>) -> HashMap<WordSequence, usize> {
+pub fn parse_file(s: &str) -> impl Iterator<Item=Vec<String>> + '_ {
+    s
+    .split(|c: char| c.is_ascii_punctuation())
+    .map(|s| parse_line(s))
+}
+
+pub fn generate_scores(sentences: impl Iterator<Item=Vec<String>>) -> HashMap<WordSequence, usize> {
     let mut prediction_map: HashMap<WordSequence, usize> = HashMap::new();
-    sentences.iter().for_each(|sentence| {
+    sentences.for_each(|sentence| {
         sentence.windows(2).for_each(|word_sequence| {
             *prediction_map
                 .entry(WordSequence(
@@ -66,11 +69,11 @@ mod tests {
 
     #[test]
     fn test_parse_file() {
-        let sentences = parse_file(TESTDATA);
-        assert_eq!(sentences[0].join(" "), "i am a fish");
-        assert_eq!(sentences[1].join(" "), "no");
-        assert_eq!(sentences[2].join(" "), "wait");
-        assert_eq!(sentences[3].join(" "), "i am a plant");
+        let mut sentences = parse_file(TESTDATA);
+        assert_eq!(sentences.next().unwrap().join(" "), "i am a fish");
+        assert_eq!(sentences.next().unwrap().join(" "), "no");
+        assert_eq!(sentences.next().unwrap().join(" "), "wait");
+        assert_eq!(sentences.next().unwrap().join(" "), "i am a plant");
     }
 
     #[test]
@@ -87,7 +90,7 @@ mod tests {
     #[test]
     fn test_bible_parser() {
         let words = parse_file(include_str!("../benches/10900-8.txt"));
-        assert_eq!(words.len(), 157387);
+        // assert_eq!(words.collect().len(), 157387);
         let scores = generate_scores(words);
         let word_predictions = group_wordpredictions(scores, 1000000);
         let word_a = word_predictions.get("a").unwrap();
