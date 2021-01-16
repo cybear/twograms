64ca9ee8a982d5b12eb80d7c8d170628bcd75645
Björn Söderqvist <bjorn.soderqvist@ingka.com>
2021-01-16 07:56:55 +0100

    Use mod for generate Still need to fix private field

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,7 @@
 use std::collections::HashMap;
 extern crate wasm_bindgen;
-use serde::Serialize;
 use serde_wasm_bindgen::to_value;
 use wasm_bindgen::prelude::*;
-
-#[derive(Serialize, Debug, Clone)]
-pub struct WordProposal<'a>(&'a str, usize);
-#[derive(Serialize, Debug, Clone, Hash)]
-pub struct WordSequence<'a>(&'a str, &'a str);
-
-impl<'a> PartialEq for WordSequence<'a> {
-    fn eq(&self, other: &Self) -> bool {
-        self.0 == other.0 && self.1 == other.1
-    }
-}
-impl<'a> Eq for WordSequence<'a> {}
 
 #[wasm_bindgen]
 pub fn to_json(text: String) -> JsValue {
@@ -22,18 +9,11 @@ pub fn to_json(text: String) -> JsValue {
     to_value(&ngrams).unwrap()
 }
 
-pub fn generate_ngrams<'a>(text: &'a str, keep: usize) -> HashMap<&'a str, Vec<WordProposal>> {
-    group_wordpredictions(generate_scores(parse::parse_file(text)), keep)
+pub fn generate_ngrams<'a>(text: &'a str, keep: usize) -> HashMap<&'a str, Vec<generate::WordProposal>> {
+    generate::group_wordpredictions(generate::generate_scores(parse::parse_file(text)), keep)
 }
 
 pub mod parse {
-    pub fn parse_line<'a>(s: &'a str) -> Vec<&'a str> {
-        s.split(|c: char| c.is_whitespace())
-            .map(|word| word.trim())
-            .filter(|word| word.len() > 0)
-            .collect()
-    }
-    
     pub fn parse_file<'a>(s: &'a str) -> Vec<&'a str> {
         s.split(|c: char| c.is_ascii_punctuation())
             .map(|sentence| sentence.trim())
@@ -42,43 +22,67 @@ pub mod parse {
     }
 }
 
+pub mod generate {
+    use std::collections::HashMap;
+    use serde::Serialize;
 
-pub fn generate_scores<'a>(sentences: Vec<&'a str>) -> HashMap<WordSequence<'a>, usize> {
-    let mut prediction_map = HashMap::new();
-    sentences.iter().for_each(|sentence| {
-        parse::parse_line(sentence).windows(2).for_each(|word_sequence| {
-            *prediction_map
-                .entry(WordSequence(&word_sequence[0], &word_sequence[1]))
-                .or_insert(0) += &1;
-        });
-    });
-    prediction_map
-}
+    #[derive(Serialize, Debug, Clone)]
+    pub struct WordProposal<'a>(&'a str, usize);
 
-pub fn group_wordpredictions<'a>(
-    predictions_hm: HashMap<WordSequence<'a>, usize>,
-    keep: usize,
-) -> HashMap<&'a str, Vec<WordProposal>> {
-    let mut hm = HashMap::new();
-    for (word_sequence, score) in predictions_hm {
-        hm.entry(word_sequence.0)
-            .or_insert(vec![])
-            .push(WordProposal(word_sequence.1, score));
+    #[derive(Serialize, Debug, Clone, Hash)]
+    pub struct WordSequence<'a>(&'a str, &'a str);
+
+    impl<'a> PartialEq for WordSequence<'a> {
+        fn eq(&self, other: &Self) -> bool {
+            self.0 == other.0 && self.1 == other.1
+        }
     }
-    // Sort the items by score descending
-    hm.into_iter()
-        .map(|(first_word, arr)| {
-            let mut sorted = arr.clone();
-            sorted.sort_by(|a, b| b.1.cmp(&a.1));
-            if sorted.len() > keep {
-                sorted.resize(
-                    keep,
-                    WordProposal("foo", 0), // This is never used
-                );
-            }
-            (first_word, sorted)
-        })
-        .collect()
+    impl<'a> Eq for WordSequence<'a> {}
+
+    pub fn parse_line<'a>(s: &'a str) -> Vec<&'a str> {
+        s.split(|c: char| c.is_whitespace())
+            .map(|word| word.trim())
+            .filter(|word| word.len() > 0)
+            .collect()
+    }
+
+    pub fn generate_scores<'a>(sentences: Vec<&'a str>) -> HashMap<WordSequence<'a>, usize> {
+        let mut prediction_map = HashMap::new();
+        sentences.iter().for_each(|sentence| {
+            parse_line(sentence).windows(2).for_each(|word_sequence| {
+                *prediction_map
+                    .entry(WordSequence(&word_sequence[0], &word_sequence[1]))
+                    .or_insert(0) += &1;
+            });
+        });
+        prediction_map
+    }
+
+    pub fn group_wordpredictions<'a>(
+        predictions_hm: HashMap<WordSequence<'a>, usize>,
+        keep: usize,
+    ) -> HashMap<&'a str, Vec<WordProposal>> {
+        let mut hm = HashMap::new();
+        for (word_sequence, score) in predictions_hm {
+            hm.entry(word_sequence.0)
+                .or_insert(vec![])
+                .push(WordProposal(word_sequence.1, score));
+        }
+        // Sort the items by score descending
+        hm.into_iter()
+            .map(|(first_word, arr)| {
+                let mut sorted = arr.clone();
+                sorted.sort_by(|a, b| b.1.cmp(&a.1));
+                if sorted.len() > keep {
+                    sorted.resize(
+                        keep,
+                        WordProposal("foo", 0), // This is never used
+                    );
+                }
+                (first_word, sorted)
+            })
+            .collect()
+    }
 }
 
 #[cfg(test)]
@@ -99,8 +103,8 @@ mod tests {
     #[test]
     fn test_hhgttg() {
         let words = parse::parse_file(include_str!("../benches/hhgttg.txt"));
-        let scores = generate_scores(words);
-        let word_predictions = group_wordpredictions(scores, 1000000);
+        let scores = generate::generate_scores(words);
+        let word_predictions = generate::group_wordpredictions(scores, 1000000);
         let word_a = word_predictions.get("a").unwrap();
         assert_eq!(word_a.len(), 585);
         assert_eq!(word_a[0].0, "small");
@@ -110,8 +114,8 @@ mod tests {
     #[test]
     fn test_bible_parser() {
         let words = parse::parse_file(include_str!("../benches/10900-8.txt"));
-        let scores = generate_scores(words);
-        let word_predictions = group_wordpredictions(scores, 1000000);
+        let scores = generate::generate_scores(words);
+        let word_predictions = generate::group_wordpredictions(scores, 1000000);
         let word_a = word_predictions.get("a").unwrap();
         assert_eq!(word_a.len(), 1333);
         assert_eq!(word_a[0].0, "man");
