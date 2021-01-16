e56b2395097c212bde0d574d5f3f725524f3a75c
Björn Söderqvist <bjorn.soderqvist@ingka.com>
2021-01-16 23:35:07 +0100

    Move tests

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,9 @@ use std::collections::HashMap;
 extern crate wasm_bindgen;
 use serde_wasm_bindgen::to_value;
 use wasm_bindgen::prelude::*;
-pub mod parse;
 pub mod generate;
+pub mod parse;
+pub mod test;
 
 #[wasm_bindgen]
 pub fn to_json(text: String) -> JsValue {
@@ -11,32 +12,9 @@ pub fn to_json(text: String) -> JsValue {
     to_value(&ngrams).unwrap()
 }
 
-pub fn generate_ngrams<'a>(text: &'a str, keep: usize) -> HashMap<&'a str, Vec<generate::WordProposal>> {
+pub fn generate_ngrams<'a>(
+    text: &'a str,
+    keep: usize,
+) -> HashMap<&'a str, Vec<generate::WordProposal>> {
     generate::group_wordpredictions(generate::generate_scores(parse::parse_file(text)), keep)
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_hhgttg() {
-        let words = parse::parse_file(include_str!("../benches/hhgttg.txt"));
-        let scores = generate::generate_scores(words);
-        let word_predictions = generate::group_wordpredictions(scores, 1000000);
-        let word_a = word_predictions.get("a").unwrap();
-        assert_eq!(word_a.len(), 585);
-        assert_eq!(word_a[0].word, "small");
-        assert_eq!(word_predictions.len(), 6045);
-    }
-
-    #[test]
-    fn test_bible_parser() {
-        let words = parse::parse_file(include_str!("../benches/10900-8.txt"));
-        let scores = generate::generate_scores(words);
-        let word_predictions = generate::group_wordpredictions(scores, 1000000);
-        let word_a = word_predictions.get("a").unwrap();
-        assert_eq!(word_a.len(), 1333);
-        assert_eq!(word_a[0].word, "man");
-    }
 }
